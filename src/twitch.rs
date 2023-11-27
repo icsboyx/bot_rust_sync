a59a739c7aaa5f16587c35b813b86142b3349aeb
icsboyx <icsboyx@blkh.lan>
2023-11-27 16:12:18 +0100

    new imprementation for capability

--- src/twitch.rs
+++ src/twitch.rs
@@ -37,6 +37,24 @@ pub struct TwitchCallbacks {
     custom_callback: Option<fn(&mut TwitchConnection, &IRCMessage)>,
 }
 
+enum TwitchCapabilitiesEnum {
+    Tags,
+    Commands,
+    Membership,
+}
+impl TwitchCapabilitiesEnum {
+    fn request(&self) -> String {
+        match self {
+            TwitchCapabilitiesEnum::Tags => "CAP REQ :twitch.tv/tags".to_string(),
+            TwitchCapabilitiesEnum::Commands => "CAP REQ :twitch.tv/commands".to_string(),
+            TwitchCapabilitiesEnum::Membership => "CAP REQ :twitch.tv/membership".to_string(),
+        }
+    }
+}
+
+struct TwitchCapabilities {
+    capabilities: Vec<TwitchCapabilitiesEnum>,
+}
 struct TwitchConnection {
     stream: TcpStream,
     callbacks: Arc<Mutex<TwitchCallbacks>>,
@@ -166,10 +184,24 @@ impl TwitchConnection {
         let mut msg = IRCMessage {
             ..Default::default()
         };
-        let message_split = message
+        let mut message_split = message
             .splitn(3, ':')
             .map(|s| s.to_owned())
             .collect::<Vec<String>>();
+
+        if message_split.len() < 3 && !message.starts_with(':') {
+            msg.context.command = message_split.get(0).unwrap_or(&"".to_string()).to_owned();
+            msg.context.sender = message_split.get(1).unwrap_or(&"".to_string()).to_owned();
+            msg.context.receiver = "*".to_string();
+            return msg;
+        }
+        if message_split.len() < 3 && message.starts_with(':') {
+            message_split.push("".to_string());
+        }
+
+        if message_split.len() < 3 && message_split[0].is_empty() {
+            return msg;
+        }
 
         msg.tags = message_split.get(0).unwrap_or(&"".to_string()).to_owned();
 
@@ -198,6 +230,11 @@ impl TwitchConnection {
 
         msg
     }
+    fn request_capabilities(&mut self, capabilities: Vec<TwitchCapabilitiesEnum>) {
+        for capability in capabilities.iter() {
+            self.send_message(capability.request().as_str());
+        }
+    }
 }
 
 pub fn run(config: Config) {
@@ -207,6 +244,13 @@ pub fn run(config: Config) {
     for channel in config.user.channels.iter() {
         twitch.join_channel(channel.as_str());
     }
+
+    twitch.request_capabilities(vec![
+        TwitchCapabilitiesEnum::Tags,
+        TwitchCapabilitiesEnum::Commands,
+        TwitchCapabilitiesEnum::Membership,
+    ]);
+
     twitch.keep_alive(60.0);
 
     twitch.callbacks.lock().unwrap().privmsg_callback = Some(my_privmsg_callback);
